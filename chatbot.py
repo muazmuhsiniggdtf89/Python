426cabdbbd65d0a4c56c9d18ebf5b1f3cc4d5a79
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-05-17 21:06:33 +0600

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,4 @@
+name=input("your name here:")
+hobby=input("what is your hobby?")
+print(f"hello {name} nice to meet you and good to know that you love {hobby} ")
+print("hello",name , "nice to meet you and good to know that you love",hobby)
