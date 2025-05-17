8267fc4ccc4cc91ec10c2af508caab59813992a6
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-05-17 21:05:39 +0600

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,35 @@
+#Let's check the datatype of different values
+a = 5
+print("type of a; ", type(a))
+
+b=2.5
+print("type of b; ", type(b))
+
+c="coding" 
+print("type of c; ", type(c))
+
+d=True 
+print("type of d; ", type(d))
+
+# Assingning Different Variables
+name = "Penguin"
+age = 15 
+is_student = True 
+weight = 38.5
+
+print("Age: ",age)
+print("Date Type of Name is", type(name))
+
+#Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is",type(weight))
+
+Str = "string"
+print(Str[::3])
+print(Str[0::1])
+print(Str[::-1])
