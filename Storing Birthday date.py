c1e0b96db8a98005e492036c92cafe2990663f6c
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-05-23 21:14:34 +0600

    Create Storing Birthday date.py

---
+++ Storing Birthday date.py
@@ -0,0 +1,12 @@
+# This are the birthday dates of Richest People in the World
+em = ("June", 28, 1971)
+jb= ("January", 12, 1964)
+mz= ("May" , 14, 1984)
+le= ("August", 17, 1944)
+bg = ("October",28,1955)
+
+print("Elon Musk's birthdate :",em)
+print("Jeff Bezos's birthdate :",jb)
+print("Mark Zuckerberg's birthdate :",mz)
+print("Larry Ellison's birthdate :",le)
+print("Bill Gates's birthdate :",bg)
