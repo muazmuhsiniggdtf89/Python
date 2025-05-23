547879311633f4f59f415f26e7ac6c444ae5160e
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-05-23 20:34:23 +0600

    Create Operating.py

---
+++ Operating.py
@@ -0,0 +1,13 @@
+# Storing Values 
+tree1 =98 
+tree2 =94 
+tree3 =41 
+tree4 =95 
+tree5 =11
+
+#Finding the total total of trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+
+#Finding the average of trees
+average = sum/5
+print("the average of all the tree is :",average)
