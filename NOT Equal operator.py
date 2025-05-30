7f3af7912949787c2b879699bd9006c4642366f3
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-05-30 20:35:01 +0600

    Create NOT Equal operator.py

---
+++ NOT Equal operator.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12 
+c = 12
+
+print( a != b)
+print( b != c)
+
+a = "python"
+b = "coding"
+print(a, "and" , b, 'are different')
+
+a = 4 
+b = 5
+
+if(a == 1 ) != (b==5):
+    print('Hello')
