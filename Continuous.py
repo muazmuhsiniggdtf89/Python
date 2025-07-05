62c21a42a236a57fae067e508065e71370fe5d6c
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-07-05 21:07:14 +0600

    Create Continuous.py

---
+++ Continuous.py
@@ -0,0 +1,10 @@
+#initialise
+var = 10
+#iterate loop
+while var > 0 :
+    var = var - 1
+    if var == 5:  #condition 1
+        continue   #continue statement
+
+    print('\nCurrent variable value :', var)
+print("\nGood bye!")
