789fb07572f0be091da89d7e5b410b9fc6ad6e0d
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-07-05 21:09:14 +0600

    Create Break.py

---
+++ Break.py
@@ -0,0 +1,10 @@
+#Take user input
+a = input("Enter a word: ")
+#program to check break keyword
+for i in a :
+    if (i =='a'):
+
+        print ("a is found")
+        break
+    else:
+        print("a not found")
