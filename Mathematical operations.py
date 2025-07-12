f1c5cc6205adea0d2d5fc1809170b0c09c23826d
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-07-12 21:11:28 +0600

    Create Mathematical operations.py

---
+++ Mathematical operations.py
@@ -0,0 +1,14 @@
+import math 
+
+print("The Floor and Ceiling value of 23.56 are:" , math.floor(23.56) , 'and', math.ceil(23.56))
+
+x = 10
+y = -15
+
+print("The value of x after copying the sign from y is:" , math.copysign(x, y))
+
+print("Absolute value of -87 and 48 are:" , math.fabs(-87), 'and' , math.fabs(48))
+
+print("The GCD of 24 and 56 is:" , math.gcd(24, 56))
+print(math.factorial(5))
+print(math.pi)
