59c66e02c72b75c512e8b87989232c5d60207c10
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-07-19 20:52:57 +0600

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,17 @@
+# Create an empty list 
+empty_list = []
+print(empty_list)
+
+# A list of numbers
+number = [1 , 2 , 3, 4 ,5]
+print(number)
+
+# Use * operator
+triples = [ 1, 2, 3,] * 3
+print(triples)
+
+# Reverse form
+
+alist = [ 100, 200, 300, 400, 500,]
+alist = alist[:: -1]
+print(alist)
