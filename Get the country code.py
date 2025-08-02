ef58ffe0bba568127d92ca0f3cff4ce32fb57d82
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-08-02 20:59:17 +0600

    Create Get the country code.py

---
+++ Get the country code.py
@@ -0,0 +1,10 @@
+country_code = {'India' : '0091', 'Australia' : '0025' , 'Nepal' : '00977'}
+
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
+
+print("Country code for Australia -")
+print(country_code.get('Australia', 'Not Found'))
