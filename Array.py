7f880c667234b63fdea3fd8d05093e75ec840c48
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-08-08 20:23:34 +0600

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,10 @@
+array_num = [1, 3, 5, 3, 7, 9, 3]
+print("Original array: ", array_num)
+
+print("First element: ", array_num[0])
+
+print("Number of occurrences of the number 3 in the said array:" +str(array_num.count(3)))
+
+array_num.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))
