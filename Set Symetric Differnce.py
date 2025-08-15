4542a37313b53238a4353a9e5b472dd5818ac846
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-08-15 20:50:10 +0600

    Create Set Symetric Differnce.py

---
+++ Set Symetric Differnce.py
@@ -0,0 +1,11 @@
+# Example A
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+symetric_difference = set1.symmetric_difference(set2)
+print("Symmetric Difference (A):", symetric_difference)
+
+# Example B
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
+symetric_difference = set1.symmetric_difference(set2)
+print("Symmetric Difference (B):", symetric_difference)
